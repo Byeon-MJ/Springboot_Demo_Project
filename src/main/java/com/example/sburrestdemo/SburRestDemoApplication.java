1498502fdfba6ace447ec32f7dd96aef8f39fa2e
Byeon-MJ <byunmj24@gmail.com>
2023-07-13 11:55:12 +0900

    export Coffee.java

--- src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
@@ -3,8 +3,6 @@ package com.example.sburrestdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.UUID;
-
 @SpringBootApplication
 public class SburRestDemoApplication {
 
@@ -12,30 +10,4 @@ public class SburRestDemoApplication {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
 
-}
-
-class Coffee {
-	private final String id;
-	private String name;
-
-	public Coffee(String id, String name){
-		this.id = id;
-		this.name = name;
-	}
-
-	public Coffee(String name){
-		this(UUID.randomUUID().toString(), name);
-	}
-
-	public String getId(){
-		return id;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public void setName(String name){
-		this.name = name;
-	}
 }
